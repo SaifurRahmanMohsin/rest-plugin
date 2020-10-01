46090947916686423ca3b9ebcae4781cc3034928
Tomaž Žitnik <tomaz.zitnik@spletna-postaj.com>
2020-10-01 12:50:36 +0200

    Slovenian language added

---
+++ lang/sl/lang.php
@@ -0,0 +1,17 @@
+<?php return [
+    'plugin' => [
+        'name' => 'RESTful',
+        'description' => 'Ustvari RESTful kontrolerje'
+    ],
+    'settings' => [
+        'name' => 'API konfiguracija',
+        'description' => 'Upravljanje API vozlišè.',
+        'prefix_label' => 'API predpona',
+        'prefix_comment' => 'Doloèi predpono, ki se bo uporabljala splošno za vsa API vozlišèa',
+        'enable_label' => 'Omogoèi vozlišèa',
+        'disable_label' => 'Onemogoèi vozlišèa',
+        'action_confirm' => 'Ali ste prepièani, da želite :action ta vozlišèa?',
+        'enable_success' => 'Uspešno omogoèena izbrana vozlišèa.',
+        'disable_success' => 'Uspešno onemogoèena izbrana vozlišèa.',
+    ]
+];
